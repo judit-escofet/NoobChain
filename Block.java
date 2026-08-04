7f02358095519ba914b06b629b0f404ff78733c4
Judit Escofet <90208829+judit-escofet@users.noreply.github.com>
2026-08-04 12:56:52 +0200

    Had problem with vscode and fixed things

--- Block.java
+++ Block.java
@@ -1,46 +1,60 @@
+package noobchain;
+
+import java.util.ArrayList;
 import java.util.Date;
 
 public class Block {
-    public String hash;
-    public String previousHash;
-    private String data;
-    private long timeStamp;
-    private int nonce;
+	
+	public String hash;
+	public String previousHash; 
+	public String merkleRoot;
+	public ArrayList<Transaction> transactions = new ArrayList<Transaction>();
+	public long timeStamp; 
+	public int nonce;
+	 
+	public Block(String previousHash ) {
+		this.previousHash = previousHash;
+		this.timeStamp = new Date().getTime();
+		
+		this.hash = calculateHash(); 
+	}
+	
+	
+	public String calculateHash() {
+		String calculatedhash = StringUtil.applySha256( 
+				previousHash +
+				Long.toString(timeStamp) +
+				Integer.toString(nonce) + 
+				merkleRoot
+				);
+		return calculatedhash;
+	}
+	
+	
+	public void mineBlock(int difficulty) {
+		merkleRoot = StringUtil.getMerkleRoot(transactions);
+		String target = StringUtil.getDificultyString(difficulty); 
+		while(!hash.substring( 0, difficulty).equals(target)) {
+			nonce ++;
+			hash = calculateHash();
+		}
+		System.out.println("Block Mined!!! : " + hash);
+	}
+	
+	
+	public boolean addTransaction(Transaction transaction) {
+		
+		if(transaction == null) return false;		
+		if((!"0".equals(previousHash))) {
+			if((transaction.processTransaction() != true)) {
+				System.out.println("Transaction failed to process. Discarded.");
+				return false;
+			}
+		}
 
-    public Block(String data, String previousHash) {
-        this.data = data;
-        this.previousHash = previousHash;
-        this.timeStamp = new Date().getTime();
-
-        this.hash = calculatedHash();
-    }
-
-    public String toString() {
-        return "Block{" +
-                "hash='" + hash + '\'' +
-                ", previousHash='" + previousHash + '\'' +
-                ", data='" + data + '\'' +
-                ", timeStamp=" + timeStamp +
-                '}';
-    }
-
-    public String calculatedHash(){
-        String calculatedHash = StringUtil.applySha256(
-            previousHash + 
-            Long.toString(timeStamp) +
-            Integer.toString(nonce) +
-            data
-        );
-        return calculatedHash;
-    }
-
-    public void mineBlock(int difficulty){
-        String target = new String(new char[difficulty]).replace('\0', '0');
-        while(!hash.substring(0, difficulty).equals(target)) {
-            nonce++;
-            hash = calculatedHash();
-        }
-        System.out.println("Block Mined!!! : " + hash);
-    }
-    
+		transactions.add(transaction);
+		System.out.println("Transaction Successfully added to Block");
+		return true;
+	}
+	
 }
